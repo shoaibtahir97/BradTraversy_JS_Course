9ade77fad74a63901a7e37dd288f9a991b9a1b4b
shoaibtahir-mydost <49276225+shoaibtahir97@users.noreply.github.com>
2023-01-17 21:32:39 +0500

    Created multi data type array

--- js_sandbox/007TemplateLiterals.js
+++ js_sandbox/007TemplateLiterals.js
@@ -15,3 +15,4 @@ html = `
 `
 
 document.body.innerHTML = html
+
---
+++ js_sandbox/008Arrays&Methods.js
@@ -0,0 +1,16 @@
+/*
+Arrays allow us to store multiple values in one variable. 
+They can be mutated and iterated through, and used in some
+complex algorithms
+*/
+
+// Creating an array
+let arrayOfNumbers = [23, 04, 12, 13, 24, 14, 56, 77]
+let arrayConstructor = new Array(12,45,12,57,54,25) //Create a constructor from array object
+const fruits = ['Orange', 'Apple', 'Banana', 'Mango'];
+const mixed = [12, 'Car', true, undefined, null, {a:4, b:9}, [24,78, 35, 69], new Date() ]
+
+let val;
+
+
+console.log(val)
