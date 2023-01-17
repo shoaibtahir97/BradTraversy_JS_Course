504612ea542884551f75fd73f9cd8742a2088e9f
shoaibtahir-mydost <49276225+shoaibtahir97@users.noreply.github.com>
2023-01-17 22:21:41 +0500

    completed till 10:36

--- js_sandbox/008Arrays&Methods.js
+++ js_sandbox/008Arrays&Methods.js
@@ -5,12 +5,45 @@ complex algorithms
 */
 
 // Creating an array
-let arrayOfNumbers = [23, 04, 12, 13, 24, 14, 56, 77]
+let numbers = [23, 04, 12, 13, 24, 14, 56, 77]
 let arrayConstructor = new Array(12,45,12,57,54,25) //Create a constructor from array object
 const fruits = ['Orange', 'Apple', 'Banana', 'Mango'];
 const mixed = [12, 'Car', true, undefined, null, {a:4, b:9}, [24,78, 35, 69], new Date() ]
 
 let val;
 
+//Get array length()
+val = numbers.length; 
 
+//Check if is an array
+val = Array.isArray(numbers) //returns boolean
+
+//Get single value of array
+val = numbers[3]
+
+//Insert in array
+numbers[2] = 100
+
+//Find index of value
+val = numbers.indexOf(23)
+
+//MUTATING ARRAYS WITH ITS METHODS
+
+//Insert value from right
+val = numbers.push(20)
+
+//Insert value from left
+val = numbers.unshift(99)
+ 
+//Remove value from right
+val = numbers.pop()
+
+//Remove value from left
+val = numbers.shift()
+
+//Remove value from center
+val = numbers.splice(1,2, 'hello') //
+
+
+console.log(numbers)
 console.log(val)
