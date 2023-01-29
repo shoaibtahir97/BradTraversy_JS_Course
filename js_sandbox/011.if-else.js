b0b867ded22895542fd403067bb3f89f01b1dc4a
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2023-01-30 00:07:03 +0500

    completed logical and or operators

--- js_sandbox/011.if-else.js
+++ js_sandbox/011.if-else.js
@@ -48,8 +48,51 @@ defined. The correct way to do this would be to use typeof operator
 //     console.log("There is no ID");
 // }
 
-if (typeof id !== undefined) {
-  console.log(`Your ID is ${id}`);
+//Testing if undefined
+// if (typeof id !== undefined) {
+//   console.log(`Your ID is ${id}`);
+// } else {
+//   console.log("THis is no ID");
+// }
+
+// //CHECKING IF A VALUE IS GREATER THAN OR LESS THAN
+// if (id > 200) {
+//   console.log("CORRECT");
+// }
+
+// ELSE IF STATEMENTS
+// const color = "orange";
+
+// if (color === "red") {
+//   console.log("color is red");
+// } else if (color === "orange") {
+//   console.log("color is orange");
+// } else if (color === "yellow") {
+//   console.log("color is yellow");
+// } else {
+//   console.log("no color found");
+// }
+
+/*
+LOGICAL OPERATORS && AND || OR
+Testing more than one value or statements
+*/
+
+// AND && Operator
+const name = "Steve";
+const age = 14;
+
+if (age > 0 && age < 12) {
+  console.log(`${name} is a child`);
+} else if (age > 12 && age <= 19) {
+  console.log(`${name} is a teenager`);
 } else {
-  console.log("THis is no ID");
+  console.log(`${name} is an adult`);
+}
+
+//OR || Operator
+if (age < 16 || age > 65) {
+  console.log(`${name} cannot run in a race`);
+} else {
+  console.log(`you are registered`);
 }
