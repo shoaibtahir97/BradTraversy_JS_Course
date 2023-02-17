1eef82f3e26816600eafc03e7f1a7f5228d194ff
Shoaib Tahir <shoaibtahir97@gmail.com>
2023-02-17 22:40:39 +0500

    updated function.js and created loops.js

--- js_sandbox/013functions.js
+++ js_sandbox/013functions.js
@@ -6,7 +6,7 @@ function greeting() {
 greeting();
 
 //Returning a value from a function and saving in variable
-function hello(){
+function hello() {
   return 'Hello'
 }
 
@@ -14,26 +14,26 @@ const greet = hello();
 console.log(greet)
 
 // Function Parameters
-function name(firstName, lastName){
+function name(firstName, lastName) {
   console.log(`Hello ${firstName} ${lastName}`)
-} 
+}
 
 name('Shoaib', 'Tahir')
 
 //Default Parameters for function if no argument is passed
 function myName(firstName = "John", lastName = "Doe") {
   console.log(`Hello ${firstName} ${lastName}`)
-}  
+}
 
-myName() 
+myName()
 //If no argument is passed and parameter is given then undefined is returned. Pass a value after the parameter.
 
 //FUNCTION EXPRESSION
 /*
 Putting a function as a variable assignment. The function is anonymous(having no name)
 */
-const square = function(x) {
-  return x*x;
+const square = function (x) {
+  return x * x;
 };
 
 console.log(square(8))
@@ -50,23 +50,23 @@ console.log(square(8))
 //   console.log('Hello '+ name);
 // })("John")
 
-//PROPERTY METHOS
+//PROPERTY METHODS
 /*
 When a function is placed inside the object it is called a method
 */
 
 let todo = {
-  add: function(){
+  add: function () {
     return 'Add Todo'
   },
-  update: function(read){
-    console.log( `Update ${read}` )
+  update: function (read) {
+    console.log(`Update ${read}`)
   }
 }
 
 console.log(todo.add());
 todo.update('Eloquient JavaScript');
-todo.delete = function(){
+todo.delete = function () {
   console.log('Delete Todo');
 };
 todo.delete();
