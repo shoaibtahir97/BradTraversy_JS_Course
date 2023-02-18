0183132cf57ae86663b08250e9785aa9b34c3412
Shoaib <shoaibtahir97@gmail.com>
2023-02-18 16:08:04 +0500

    added date file in staging

--- js_sandbox/010Dates&Times.js
+++ js_sandbox/010Dates&Times.js
@@ -1,21 +1,21 @@
 let date = new Date()
-let birthday = new Date('10/27/1997 02:45:59');
-let val;
+// let birthday = new Date('10/27/1997 02:45:59');
+// let val;
 
 
 val = date.getDate();
-val = date.getMonth()
-val = date.getDay();
-val = date.getFullYear()
-val = date.getHours()
-val = date.getMinutes()
-val = date.getSeconds()
-val = date.getMilliseconds()
+// val = date.getMonth()
+// val = date.getDay();
+// val = date.getFullYear()
+// val = date.getHours()
+// val = date.getMinutes()
+// val = date.getSeconds()
+// val = date.getMilliseconds()
 
-val = date.setMonth(10)
-val = date.setDate(15)
-val = date.setFullYear(2003)
+// val = date.setMonth(10)
+// val = date.setDate(15)
+// val = date.setFullYear(2003)
 
 // console.log(birthday)
-console.log('date --->', date)
+console.log('date --->',date.now())
 console.log('val --->', val)
